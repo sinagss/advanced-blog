c05499265793ef9e20cfd1a3fc42c48fe687bbd6
sinagss <sina.ghassai@gmail.com>
2023-05-12 18:19:30 +0330

    fix hook

--- src/hooks/usePersistedState.js
+++ src/hooks/usePersistedState.js
@@ -1,19 +1,17 @@
 import { useState, useEffect } from "react";
 
-const usePersistedStete = (storageKey, defaultValue) => {
+export function usePersistedState(storageKey, defaultValue) {
   const [isCached, setIsCached] = useState(
     Boolean(localStorage.getItem(storageKey))
   );
 
   const [value, setValue] = useState(
-    isCached ? localStorage.getItem(storageKey) : defaultValue
+    isCached ? JSON.parse(localStorage.getItem(storageKey)) : defaultValue
   );
 
   useEffect(() => {
     localStorage.setItem(storageKey, JSON.stringify(value));
   }, [value]);
 
-  return { value, setValue, isCached };
-};
-
-export default usePersistedStete;
+  return [value, setValue, isCached];
+}
