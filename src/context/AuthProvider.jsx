e3398a0f7d3d27ee22b53b86c7687bae49bc8f37
sinagss <sina.ghassai@gmail.com>
2023-05-13 19:21:18 +0330

    finished Authentication Context

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,13 +1,48 @@
-import React, { useState } from "react";
-import { setItem, removeItem, getItem } from "../hooks/useLocalStorage";
+import { usePersistedState } from "../hooks/usePersistedState";
 import AuthContext from "./AuthContext";
+import { users } from "../data/users";
+import storageKeys from "../constants/storageKeys";
 
 const AuthProvider = ({ children }) => {
-  const [isLogedIn, setIsLoggedIn] = useState(
-    getItem("logedIn") ? true : false
+  const [loggedIn, setIsLoggedIn] = usePersistedState(
+    storageKeys.isLogedin,
+    false
+  );
+  const [username, setUsername] = usePersistedState(storageKeys.username, []);
+  const [accessLevel, setAccessLevel] = usePersistedState(
+    storageKeys.accessLevel,
+    []
   );
 
-  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+  function login(username, password) {
+    for (const user of users) {
+      if (username === user.user && password === user.password) {
+        setIsLoggedIn(true);
+        setUsername(username);
+        setAccessLevel(user.accessLevel);
+      }
+    }
+  }
+
+  function logout() {
+    setIsLoggedIn(false);
+    setUsername("");
+    setAccessLevel(2);
+  }
+
+  const providerValues = {
+    login: login,
+    logout: logout,
+    username: username,
+    loggedIn: loggedIn,
+    accessLevel: accessLevel,
+  };
+
+  return (
+    <AuthContext.Provider value={providerValues}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
